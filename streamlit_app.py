83e2f1b447e6fb754216b5dcc0b3ca2eaf0411e1
ttangtraining <129430633+ttangtraining@users.noreply.github.com>
2023-04-04 11:43:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,9 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
+#stop functions
+streamlit.stop()
+
 # Allow end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
